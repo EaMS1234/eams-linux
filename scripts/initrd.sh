af52227a32157bfa49d70f6e4fb296690d7b4b7c
Erick Augusto <contato@eams.dev.br>
2026-07-30 13:31:44 -0300

    Changed ownership of initrd files

--- scripts/initrd.sh
+++ scripts/initrd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd initrd
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../root/boot/initrd.img
+find . -print0 | cpio --null -ov --format=newc --owner 0:0 | gzip -9 > ../root/boot/initrd.img
 cd ..
 
