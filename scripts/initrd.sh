46816d1c83ca990daceb24c46a921f7b011c548a
Erick Augusto <contato@eams.dev.br>
2026-08-02 12:09:01 -0300

    Fixed file permissions in initrd/etc

--- scripts/initrd.sh
+++ scripts/initrd.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+chmod 600 initrd/etc/shadow
+chmod 400 initrd/etc/doas.conf
+
 cd initrd
 find . -print0 | cpio --null -ov --format=newc --owner 0:0 | gzip -9 > ../root/boot/initrd.img
 cd ..
+
+chmod 644 initrd/etc/shadow initrd/etc/doas.conf
 
