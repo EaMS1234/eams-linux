39658046d3896f0eacfaa1720371bbbae6e7cfa1
Erick Augusto <contato@eams.dev.br>
2026-08-05 18:37:58 -0300

    Added wayland-protocols

--- scripts/wayland.sh
+++ scripts/wayland.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 WAYLAND=wayland-1.26.0
+WAYLAND_PROTOCOLS=wayland-protocols-1.49
 PREFIX=$(pwd)/initrd
 
 if [ ! -d $WAYLAND ]; then
@@ -23,5 +24,28 @@ fi
 cd $WAYLAND
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 CC=musl-gcc meson setup build --prefix=$PREFIX -Ddocumentation=false
+ninja -C build install
+cd ..
+
+
+if [ ! -d $WAYLAND_PROTOCOLS ]; then
+  if [ ! -e $WAYLAND_PROTOCOLS.tar.gz ]; then
+    wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/archive/1.49/$WAYLAND_PROTOCOLS.tar.gz
+
+  else
+    echo "$WAYLAND_PROTOCOLS.tar.gz is already present"
+
+  fi
+
+  tar -xf $WAYLAND_PROTOCOLS.tar.gz
+  rm $WAYLAND_PROTOCOLS.tar.gz
+
+else
+  echo "$WAYLAND_PROTOCOLS is already present"
+
+fi
+
+cd $WAYLAND_PROTOCOLS
+meson setup build --prefix=$PREFIX
 ninja -C build install
 cd ..
