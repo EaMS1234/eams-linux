40414a44a29224f5426b66969deec577024b24ec
Erick Augusto <contato@eams.dev.br>
2026-08-12 17:00:43 -0300

    Added Dockerfile for faster building with scripts/make_with_docker.sh

--- scripts/make_with_docker.sh
+++ scripts/make_with_docker.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-TOOLS="build-base clang lld llvm mtools nasm bc cpio gettext perl xorriso meson ninja git bison cmake rsync gzip flex diffutils findutils wget ca-certificates ssl_client"
-LIBS="linux-headers pkgconf pkgconf-dev libffi-dev expat-dev libxml2-dev xkeyboard-config elfutils-dev openssl-dev ncurses-dev"
+if [ -z $(docker images -q "eams-linux-build:latest" 2> /dev/null) ]; then
+  docker build -t "eams-linux-build:latest" scripts/
+fi
 
-docker run --rm -it -v $(pwd):/eams-linux/ alpine \
-  sh -c "apk update && apk add $TOOLS $LIBS && cd /eams-linux/ && make $1 eams-linux.iso && chown -R $(id -u):$(id -g) /eams-linux/"
+docker run --rm -it -v $(pwd):/eams-linux/ "eams-linux-build:latest" \
+  sh -c "cd /eams-linux/ && make $1 eams-linux.iso && chown -R $(id -u):$(id -g) /eams-linux/"
 
